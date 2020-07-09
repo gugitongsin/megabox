ce22fe3b4f0b8208053969f46f47c64287500fee
jskee <gugitongsin@gmail.com>
2020-07-09 15:19:37 +0900

    0709

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(function(){
+
+    $(document).on('click', 'a[href="#"]', function(e){
+        e.preventDefault();
+    })    
+    //btn_close .onclick
+    $('.btn_close').on('click', function(){
+        $(this).parent().addClass('close');
+    })
+
+    //header .onmouseenter
+    $('header').on('mouseleave', function(){
+        $('header').removeClass('on');
+        $('header > .inner > .gnb > ul > div > li > a').removeClass('on');
+        $('header > .inner > .gnb > ul > div > li > ul').removeClass('on');
+    });
+    $('header > .inner > .gnb > ul > div > li > a').on('mouseenter',function(){
+        $('header > .inner > .gnb > ul > div > li > a').removeClass('on');
+        $('header > .inner > .gnb > ul > div > li > ul').removeClass('on');
+        $(this).parent().find('ul').addClass('on');
+        $(this).addClass('on');
+        $('header').addClass('on');
+        if($(this).hasClass('empty') === true){
+            $('header').removeClass('on');
+            $(this).removeClass('on');
+        }
+    });
+
+    
+})
